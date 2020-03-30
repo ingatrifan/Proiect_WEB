a505ac1f9917fa64b28314d117694cc6f7ee0cd3
Inga <ingaa.trifan@gmail.com>
2020-03-30 20:48:52 +0300

    database connection+ models

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,30 @@
 //for demonstrative purposes
 const http = require('http');
 const url = require('url');
+const httpStatusCode = require('http-status-codes');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+//MOngo connection
+mongoose
+  .connect("mongodb+srv://admin:admin@twproject-skgsk.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("successfully connected to database"));
+
+//accesing models
+const db = require("./models");
 
 const endpointUtils = require('./routes/endpoint_utilities');
+
+//testing database connection
+const testDatabase = async()=>{
+    const user = await db.User.create({email:"first",password:"second"});
+    console.log(user);
+    const file = await db.File.create({name:"file1",path:"foof.com"});
+    console.log(file);
+}
+testDatabase();
 
 const server =http.createServer( (req,res)=>{
     try {
@@ -12,7 +34,7 @@ const server =http.createServer( (req,res)=>{
         let handler = endpointUtils.getHandler(method,pathName);
         handler(req,res);
     }catch(err) {
-        res.statusCode = 500;
+        res.statusCode = httpStatusCode.INTERNAL_SERVER_ERROR;
         res.end();
     }   
 });

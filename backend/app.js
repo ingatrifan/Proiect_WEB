d089bbede1ba593cbe2f3ec2e0fbc8c3f04d814f
kreL-Lo <54546232+kreL-Lo@users.noreply.github.com>
2020-04-06 21:11:10 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -1,55 +1,72 @@
-//for demonstrative purposes
+//libs
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const url = require('url');
-const httpStatusCode = require('http-status-codes');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-//Mongo connection
-mongoose
-  .connect("mongodb+srv://admin:admin@twproject-skgsk.mongodb.net/test?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
+//built in
+const rendering = require('./routes/pageRendering');
+const controllers = require('./controllers/index');
+const models = require('./models/index');
+
+
+//configuration
+const config =dotenv.config({
+  path: './config/config.env'}).parsed;
+//connect db
+//NOTE: ADD YOUR IP ADDRESS TO MONGO DB 
+async function connectDB(){
+  await mongoose.connect(config.MONGO_URI,
+    {
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+  }).then(()=>console.log("Connected to DB")).catch((err)=>{
+    console.log(err);
   })
-  .then(() => console.log("successfully connected to database"));
-
-//accesing models
-const db = require("./models");
-//accesing controllers
-const controllers = require("./controllers");
-//controllers.authController.login()
-
-
-const endpointUtils = require('./routes/endpoint_utilities');
-
-//testing database connection
-const testDatabase = async()=>{
-    const user = await db.User.create({email:"first",password:"second"});
-    console.log(user);
-    const file = await db.File.create({name:"file1",path:"foof.com"});
-    console.log(file);
+  //Line command that should be deleted some day
+  await models.User.remove({},()=>console.log("Cleaning testing"));
+  //adding test user 
+  let b = new models.User({
+    email:'test@gmail.com',
+    name :'test',
+    password :'asdf'
+  });
+  b.save(function(){
+    console.log("inserted test rat");
+  });
 }
-//testDatabase();
+connectDB();
 
-const server =http.createServer( (req,res)=>{
-    try {
-        let reqUrlString = req.url;
-        let pathName = url.parse(reqUrlString,true,false);
-        let method = req.method;
-        let handler = endpointUtils.getHandler(method,pathName);
-        handler(req,res);
-    }catch(err) {
-        res.statusCode = httpStatusCode.INTERNAL_SERVER_ERROR;
-        res.end();
-    }   
+const port = 3000;
+const server = http.createServer((req, res) => {
+  //FOR SOME ERRORS 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    if(req.method==='GET'){
+      rendering.pageRendering(res,req);
+    }
+    else if(req.method==='POST')
+    {
+      var pathUrl = url.parse(req.url).pathname.split('/').reverse()[0];
+      if(pathUrl =='login')
+      {
+        controllers.login.login(req,res);
+      }
+      else if(pathUrl=='register'){
+        controllers.register.register(req,res);
+        //handle register
+      }
+      
+
+    }
+    else{
+      res.statusCode=404;
+      res.end();
+    }
 });
 
-process.on("uncaughtException",(err) =>{
-    console.log("Caught error",err);
+server.listen(config.PORT, config.HOSTNAME, () => {
+  console.log(`Server running at http://${config.HOSTNAME}:${config.PORT}`);
 });
-
-const PORT = 3000;
-server.listen(PORT);
-console.log('Listening on port: ',PORT);
-
-
